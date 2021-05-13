cc13d08abf3aa2b200011bce504362aa6ce5f677
arara90 <ararajo@gmail.com>
2021-05-13 12:36:00 +0900

    auth module passport

--- iluvcoffee/src/auth/auth.module.ts
+++ iluvcoffee/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { AuthService } from './auth.service'
+import {UsersModule} from "../users/users.module";
+
 
 
 @Module({
-  controllers: [AuthController],
+  controllers: [UsersModule],
   providers: [AuthService]
 })
 export class AuthModule {}
